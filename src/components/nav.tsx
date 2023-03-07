3fe3ec504a2847bdc186c94025a89c86c8bf5d44
Indifferent98 <zhenyayarmokova@mail.ru>
2023-03-07 23:27:29 +0300

    AddCssFileForComponent

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "./nav.css";
 const Nav = (): JSX.Element => {
   return (
     <nav className="nav">
-      <a href="#" className="block">
+      <a href="#" className="item">
         Profile
       </a>
-      <a href="#" className="block">
+      <a href="#" className="item">
         Messages
       </a>
-      <a href="#" className="block">
+      <a href="#" className="item">
         News
       </a>
-      <a href="#" className="block">
+      <a href="#" className="item">
         Music
       </a>
     </nav>
