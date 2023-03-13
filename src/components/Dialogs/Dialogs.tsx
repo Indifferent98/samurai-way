0c408d9d214a9510b1f537ec09320ed94cdbc016
Indifferent98 <zhenyayarmokova@mail.ru>
2023-03-14 01:42:08 +0300

    add dialog wrapper

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,48 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 export const Dialogs = () => {
-  return <h1>Dialogs</h1>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialog}>
+        <NavLink
+          to="/Dialogs/Timur"
+          className={s.dialogItems}
+          activeClassName={s.active}
+        >
+          Timur
+        </NavLink>
+        <NavLink
+          to="/Dialogs/Vladimir"
+          className={s.dialogItems}
+          activeClassName={s.active}
+        >
+          Vladimir
+        </NavLink>
+        <NavLink
+          to="/Dialogs/Maxim"
+          activeClassName={s.active}
+          className={s.dialogItems}
+        >
+          Maxim
+        </NavLink>
+        <NavLink to="/Dialogs/Igor" className={s.dialogItems}>
+          Igor
+        </NavLink>
+        <NavLink
+          to="/Dialogs/Stepan"
+          className={s.dialogItems}
+          activeClassName={s.active}
+        >
+          Stepan
+        </NavLink>
+      </div>
+
+      <div className={s.messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>How Are You</div>
+        <div className={s.message}>How is your Learning</div>
+      </div>
+    </div>
+  );
 };
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import "./nav.module.css";
+import s from "./nav.module.css";
 import { NavLink } from "react-router-dom";
-const s = { nav: "nav_nav__3Z1IF", item: "nav_item__fwMVs" };
+// const s = { nav: "nav_nav__3Z1IF", item: "nav_item__fwMVs" };
 const Nav = (): JSX.Element => {
   return (
     <nav className={s.nav}>
-      <NavLink to="/Content" className={s.item}>
+      <NavLink to="/Content" activeClassName={s.activeLink} className={s.item}>
         Profile
       </NavLink>
-      <NavLink to="/Dialogs" className={s.item}>
+      <NavLink to="/Dialogs" activeClassName={s.activeLink} className={s.item}>
         Messages
       </NavLink>
-      <NavLink to="/News" className={s.item}>
+      <NavLink to="/News" activeClassName={s.activeLink} className={s.item}>
         News
       </NavLink>
-      <NavLink to="/Music" className={s.item}>
+      <NavLink to="/Music" activeClassName={s.activeLink} className={s.item}>
         Music
       </NavLink>
-      <NavLink to="/Settings" className={s.item}>
+      <NavLink to="/Settings" activeClassName={s.activeLink} className={s.item}>
         Settings
       </NavLink>
     </nav>
